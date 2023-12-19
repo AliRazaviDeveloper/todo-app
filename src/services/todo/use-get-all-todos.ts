0a243a915042ea975a9f64c99f100fdcef521d1e
alirazavi <alirazavi253@gmail.com>
2023-12-20 00:36:13 +0330

    feat: service get all todos

---
+++ src/services/todo/use-get-all-todos.ts
@@ -0,0 +1,39 @@
+import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import api from "../../utility/server";
+import { TParams } from "../../types/global";
+import { IOutput } from "../../types/todo";
+
+type TUseGetTodosQuery = {
+  todos: IOutput["data"]["items"] | undefined;
+  isError: boolean;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isFetched: boolean;
+  refetch: () => Promise<unknown>;
+};
+
+const getAllTodos = (params?: TParams) =>
+  api.get("/todos", {
+    params,
+  });
+
+export const useGetTodosQuery = (params?: TParams): TUseGetTodosQuery => {
+  const { isError, isLoading, data, isSuccess, refetch, isFetched } = useQuery<
+    AxiosResponse<IOutput["data"]["items"]>
+  >({
+    queryKey: ["get.todos"],
+    queryFn: () => getAllTodos(params),
+  });
+
+  return {
+    todos: data?.data,
+    isError,
+    isLoading,
+    isSuccess,
+    isFetched,
+    refetch,
+  };
+};
+
+export default useGetTodosQuery;
