ef022c822be746d11ff061b58c0a1c3ee3fec519
alirazavi <alirazavi253@gmail.com>
2023-12-20 00:35:10 +0330

    feat: react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import { ThemeSettings } from "./theme/Theme.ts";
 import { useRoutes } from "react-router-dom";
 import Router from "./routes/Router.tsx";
 import ScrollToTop from "./components/share/scrollToTop/ScrollToTop.tsx";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const App = (): JSX.Element => {
   const routing = useRoutes(Router);
   const theme = ThemeSettings();
+  const queryClient = new QueryClient();
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <ScrollToTop>{routing}</ScrollToTop>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ScrollToTop>{routing}</ScrollToTop>
+      </ThemeProvider>
+      <Toaster />
+    </QueryClientProvider>
   );
 };
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,20 @@
-import React, {Suspense} from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./store/Store.ts";
-import {CircularProgress} from "@mui/material";
+import { CircularProgress } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <Suspense fallback={<CircularProgress/>}>
-                <BrowserRouter>
-                    <App/>
-                </BrowserRouter>
-            </Suspense>
-
-        </React.StrictMode>
-    </Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <Suspense fallback={<CircularProgress />}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Suspense>
+    </React.StrictMode>
+  </Provider>
+);
